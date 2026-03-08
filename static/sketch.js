4df9a1beee81e27ee5ae288b57be7a9fc5c95715
enderman1472580-netizen <enderman1472580@gmail.com>
2026-03-08 07:31:41 +0700

    Update sketch.js

--- static/sketch.js
+++ static/sketch.js
@@ -20,7 +20,7 @@ function windowResized() {
 }
 
 function preload() {
-  music = loadSound("/static/thoikhong.mp3");
+  music = loadSound("static/thoikhong.mp3");
 }
 
 function setup() {
@@ -267,3 +267,4 @@ function showFinalWish() {
     }, 50);
   }
 }
+
